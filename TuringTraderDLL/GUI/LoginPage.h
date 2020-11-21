74c1018d658d8e2618897b5f08f5238388f9b87e
Liam Fiebig <lfiebig11@gmail.com>
2020-11-21 18:41:05 -0500

    Added Global Variable for current User

--- TuringTraderDLL/GUI/LoginPage.h
+++ TuringTraderDLL/GUI/LoginPage.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "TestGUI.h"
 #include "MainGUI.h"
+#include <msclr\marshal_cppstd.h>
+
+string currentUser = "test";
 
 namespace GUI {
 	using namespace AppGUI;
@@ -11,6 +14,7 @@ namespace GUI {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace msclr::interop;
 
 	/// <summary>
 	/// Summary for LoginPage
@@ -55,7 +59,7 @@ namespace GUI {
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -85,9 +89,9 @@ namespace GUI {
 			this->loginTitle->Controls->Add(this->subtitleLabel);
 			this->loginTitle->Dock = System::Windows::Forms::DockStyle::Left;
 			this->loginTitle->Location = System::Drawing::Point(0, 0);
-			this->loginTitle->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->loginTitle->Margin = System::Windows::Forms::Padding(6, 4, 6, 4);
 			this->loginTitle->Name = L"loginTitle";
-			this->loginTitle->Size = System::Drawing::Size(437, 700);
+			this->loginTitle->Size = System::Drawing::Size(874, 1277);
 			this->loginTitle->TabIndex = 20;
 			// 
 			// titleLabel
@@ -95,9 +99,10 @@ namespace GUI {
 			this->titleLabel->AutoSize = true;
 			this->titleLabel->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 28.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->titleLabel->Location = System::Drawing::Point(55, 58);
+			this->titleLabel->Location = System::Drawing::Point(110, 112);
+			this->titleLabel->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
 			this->titleLabel->Name = L"titleLabel";
-			this->titleLabel->Size = System::Drawing::Size(302, 55);
+			this->titleLabel->Size = System::Drawing::Size(596, 108);
 			this->titleLabel->TabIndex = 9;
 			this->titleLabel->Text = L"TuringTrader";
 			// 
@@ -106,9 +111,10 @@ namespace GUI {
 			this->subtitleLabel->AutoSize = true;
 			this->subtitleLabel->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->subtitleLabel->Location = System::Drawing::Point(19, 148);
+			this->subtitleLabel->Location = System::Drawing::Point(38, 287);
+			this->subtitleLabel->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
 			this->subtitleLabel->Name = L"subtitleLabel";
-			this->subtitleLabel->Size = System::Drawing::Size(401, 32);
+			this->subtitleLabel->Size = System::Drawing::Size(788, 63);
 			this->subtitleLabel->TabIndex = 10;
 			this->subtitleLabel->Text = L" A CISC 320 Trading Simulator";
 			// 
@@ -117,9 +123,10 @@ namespace GUI {
 			this->CreateAccount->AutoSize = true;
 			this->CreateAccount->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->CreateAccount->Location = System::Drawing::Point(905, 392);
+			this->CreateAccount->Location = System::Drawing::Point(1810, 760);
+			this->CreateAccount->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
 			this->CreateAccount->Name = L"CreateAccount";
-			this->CreateAccount->Size = System::Drawing::Size(148, 25);
+			this->CreateAccount->Size = System::Drawing::Size(297, 46);
 			this->CreateAccount->TabIndex = 19;
 			this->CreateAccount->TabStop = true;
 			this->CreateAccount->Text = L"Create Account";
@@ -129,9 +136,10 @@ namespace GUI {
 			this->loginLabel->AutoSize = true;
 			this->loginLabel->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->loginLabel->Location = System::Drawing::Point(582, 123);
+			this->loginLabel->Location = System::Drawing::Point(1164, 238);
+			this->loginLabel->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
 			this->loginLabel->Name = L"loginLabel";
-			this->loginLabel->Size = System::Drawing::Size(445, 51);
+			this->loginLabel->Size = System::Drawing::Size(896, 97);
 			this->loginLabel->TabIndex = 18;
 			this->loginLabel->Text = L"Login to Turing Trader";
 			// 
@@ -141,10 +149,10 @@ namespace GUI {
 			this->loginBtn->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 19.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->loginBtn->ForeColor = System::Drawing::Color::GhostWhite;
-			this->loginBtn->Location = System::Drawing::Point(565, 470);
-			this->loginBtn->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->loginBtn->Location = System::Drawing::Point(1130, 911);
+			this->loginBtn->Margin = System::Windows::Forms::Padding(6, 4, 6, 4);
 			this->loginBtn->Name = L"loginBtn";
-			this->loginBtn->Size = System::Drawing::Size(181, 65);
+			this->loginBtn->Size = System::Drawing::Size(362, 126);
 			this->loginBtn->TabIndex = 17;
 			this->loginBtn->Text = L"Login";
 			this->loginBtn->UseVisualStyleBackColor = false;
@@ -155,21 +163,23 @@ namespace GUI {
 			this->forgotPassword->AutoSize = true;
 			this->forgotPassword->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->forgotPassword->Location = System::Drawing::Point(561, 392);
+			this->forgotPassword->Location = System::Drawing::Point(1122, 760);
+			this->forgotPassword->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
 			this->forgotPassword->Name = L"forgotPassword";
-			this->forgotPassword->Size = System::Drawing::Size(170, 25);
+			this->forgotPassword->Size = System::Drawing::Size(345, 46);
 			this->forgotPassword->TabIndex = 16;
 			this->forgotPassword->TabStop = true;
 			this->forgotPassword->Text = L"Forgot Password\?";
+			this->forgotPassword->LinkClicked += gcnew System::Windows::Forms::LinkLabelLinkClickedEventHandler(this, &LoginPage::forgotPassword_LinkClicked);
 			// 
 			// passwordInput
 			// 
 			this->passwordInput->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 19.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->passwordInput->Location = System::Drawing::Point(785, 313);
-			this->passwordInput->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->passwordInput->Location = System::Drawing::Point(1570, 606);
+			this->passwordInput->Margin = System::Windows::Forms::Padding(6, 4, 6, 4);
 			this->passwordInput->Name = L"passwordInput";
-			this->passwordInput->Size = System::Drawing::Size(267, 45);
+			this->passwordInput->Size = System::Drawing::Size(530, 82);
 			this->passwordInput->TabIndex = 15;
 			// 
 			// password
@@ -177,9 +187,10 @@ namespace GUI {
 			this->password->AutoSize = true;
 			this->password->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 19.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->password->Location = System::Drawing::Point(558, 320);
+			this->password->Location = System::Drawing::Point(1116, 620);
+			this->password->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
 			this->password->Name = L"password";
-			this->password->Size = System::Drawing::Size(171, 38);
+			this->password->Size = System::Drawing::Size(341, 75);
 			this->password->TabIndex = 14;
 			this->password->Text = L"Password:";
 			// 
@@ -187,28 +198,30 @@ namespace GUI {
 			// 
 			this->usernameInput->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 19.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->usernameInput->Location = System::Drawing::Point(785, 237);
-			this->usernameInput->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->usernameInput->Location = System::Drawing::Point(1570, 459);
+			this->usernameInput->Margin = System::Windows::Forms::Padding(6, 4, 6, 4);
 			this->usernameInput->Name = L"usernameInput";
-			this->usernameInput->Size = System::Drawing::Size(267, 45);
+			this->usernameInput->Size = System::Drawing::Size(530, 82);
 			this->usernameInput->TabIndex = 13;
+			this->usernameInput->TextChanged += gcnew System::EventHandler(this, &LoginPage::usernameInput_TextChanged);
 			// 
 			// username
 			// 
 			this->username->AutoSize = true;
 			this->username->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 19.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->username->Location = System::Drawing::Point(558, 237);
+			this->username->Location = System::Drawing::Point(1116, 459);
+			this->username->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
 			this->username->Name = L"username";
-			this->username->Size = System::Drawing::Size(177, 38);
+			this->username->Size = System::Drawing::Size(356, 75);
 			this->username->TabIndex = 12;
 			this->username->Text = L"Username:";
 			// 
 			// LoginPage
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(16, 31);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1200, 700);
+			this->ClientSize = System::Drawing::Size(2372, 1277);
 			this->Controls->Add(this->loginTitle);
 			this->Controls->Add(this->CreateAccount);
 			this->Controls->Add(this->loginLabel);
@@ -218,7 +231,8 @@ namespace GUI {
 			this->Controls->Add(this->password);
 			this->Controls->Add(this->usernameInput);
 			this->Controls->Add(this->username);
-			this->MaximumSize = System::Drawing::Size(1218, 747);
+			this->Margin = System::Windows::Forms::Padding(6);
+			this->MaximumSize = System::Drawing::Size(2404, 1365);
 			this->Name = L"LoginPage";
 			this->Text = L"LoginPage";
 			this->Load += gcnew System::EventHandler(this, &LoginPage::LoginPage_Load);
@@ -232,10 +246,16 @@ namespace GUI {
 	private: System::Void LoginPage_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
 	private: System::Void loginBtn_Click(System::Object^ sender, System::EventArgs^ e) {
+		String^ s = usernameInput->Text;
+		currentUser = marshal_as<string>(s);
 		MainGUI^ test = gcnew MainGUI();
 		this->Hide();
 		test->ShowDialog();
 		this->Show();
 	}
-};
+	private: System::Void forgotPassword_LinkClicked(System::Object^ sender, System::Windows::Forms::LinkLabelLinkClickedEventArgs^ e) {
+	}
+	private: System::Void usernameInput_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	};
 }
--- TuringTraderDLL/GUI/ProfilePage.h
+++ TuringTraderDLL/GUI/ProfilePage.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <msclr\marshal_cppstd.h>
+
+extern std::string currentUser;
 
 namespace ProfileGUI {
 
@@ -8,6 +11,7 @@ namespace ProfileGUI {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace msclr::interop;
 
 	/// <summary>
 	/// Summary for ProfilePage
@@ -50,6 +54,10 @@ namespace ProfileGUI {
 	private: System::Windows::Forms::Label^ label63;
 	private: System::Windows::Forms::Label^ label62;
 	private: System::Windows::Forms::Label^ label61;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox4;
 
 	protected:
 
@@ -92,7 +100,7 @@ namespace ProfileGUI {
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -116,15 +124,19 @@ namespace ProfileGUI {
 			this->label63 = (gcnew System::Windows::Forms::Label());
 			this->label62 = (gcnew System::Windows::Forms::Label());
 			this->label61 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
 			// 
 			// button12
 			// 
 			this->button12->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
-			this->button12->Location = System::Drawing::Point(934, 637);
-			this->button12->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->button12->Location = System::Drawing::Point(1868, 1234);
+			this->button12->Margin = System::Windows::Forms::Padding(6, 4, 6, 4);
 			this->button12->Name = L"button12";
-			this->button12->Size = System::Drawing::Size(264, 53);
+			this->button12->Size = System::Drawing::Size(528, 103);
 			this->button12->TabIndex = 35;
 			this->button12->Text = L"Reset account";
 			this->button12->UseVisualStyleBackColor = true;
@@ -133,9 +145,10 @@ namespace ProfileGUI {
 			// 
 			this->label70->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
 			this->label70->AutoSize = true;
-			this->label70->Location = System::Drawing::Point(839, 503);
+			this->label70->Location = System::Drawing::Point(1678, 975);
+			this->label70->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
 			this->label70->Name = L"label70";
-			this->label70->Size = System::Drawing::Size(363, 68);
+			this->label70->Size = System::Drawing::Size(719, 128);
 			this->label70->TabIndex = 33;
 			this->label70->Text = L"Reset account completely, removing all\r\ncurrent positions and resetting account b"
 				L"alance to base.\r\n\r\nWould you like to continue\?\r\n";
@@ -145,9 +158,10 @@ namespace ProfileGUI {
 			this->label69->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
 			this->label69->AutoSize = true;
 			this->label69->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18));
-			this->label69->Location = System::Drawing::Point(974, 441);
+			this->label69->Location = System::Drawing::Point(1948, 854);
+			this->label69->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
 			this->label69->Name = L"label69";
-			this->label69->Size = System::Drawing::Size(210, 36);
+			this->label69->Size = System::Drawing::Size(418, 69);
 			this->label69->TabIndex = 32;
 			this->label69->Text = L"Reset Account";
 			// 
@@ -157,9 +171,10 @@ namespace ProfileGUI {
 			this->label68->AutoSize = true;
 			this->label68->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 7.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label68->Location = System::Drawing::Point(866, 322);
+			this->label68->Location = System::Drawing::Point(1732, 624);
+			this->label68->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
 			this->label68->Name = L"label68";
-			this->label68->Size = System::Drawing::Size(371, 51);
+			this->label68->Size = System::Drawing::Size(703, 93);
 			this->label68->TabIndex = 31;
 			this->label68->Text = L"Turing Trading 101\r\nLorem ipsum dolor sit amet. Ut consequat.\r\nNullam quis est eg"
 				L"et eros faucibus volutpat ut eget dolor. ";
@@ -170,9 +185,10 @@ namespace ProfileGUI {
 			this->label67->AutoSize = true;
 			this->label67->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 7.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label67->Location = System::Drawing::Point(866, 189);
+			this->label67->Location = System::Drawing::Point(1732, 366);
+			this->label67->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
 			this->label67->Name = L"label67";
-			this->label67->Size = System::Drawing::Size(186, 51);
+			this->label67->Size = System::Drawing::Size(353, 93);
 			this->label67->TabIndex = 30;
 			this->label67->Text = L"Stock Trading 101\r\nLorem ipsum dolor sit amet. \r\nNullam quis eget dolor. ";
 			// 
@@ -182,9 +198,10 @@ namespace ProfileGUI {
 			this->label66->AutoSize = true;
 			this->label66->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 19.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label66->Location = System::Drawing::Point(1022, 111);
+			this->label66->Location = System::Drawing::Point(2044, 215);
+			this->label66->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
 			this->label66->Name = L"label66";
-			this->label66->Size = System::Drawing::Size(84, 38);
+			this->label66->Size = System::Drawing::Size(168, 75);
 			this->label66->TabIndex = 29;
 			this->label66->Text = L"Help";
 			// 
@@ -193,48 +210,49 @@ namespace ProfileGUI {
 			this->label65->AutoSize = true;
 			this->label65->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 19.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label65->Location = System::Drawing::Point(269, 111);
+			this->label65->Location = System::Drawing::Point(538, 215);
+			this->label65->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
 			this->label65->Name = L"label65";
-			this->label65->Size = System::Drawing::Size(109, 38);
+			this->label65->Size = System::Drawing::Size(218, 75);
 			this->label65->TabIndex = 28;
 			this->label65->Text = L"Profile";
 			// 
 			// button11
 			// 
-			this->button11->Location = System::Drawing::Point(435, 601);
-			this->button11->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->button11->Location = System::Drawing::Point(870, 1164);
+			this->button11->Margin = System::Windows::Forms::Padding(6, 4, 6, 4);
 			this->button11->Name = L"button11";
-			this->button11->Size = System::Drawing::Size(191, 48);
+			this->button11->Size = System::Drawing::Size(382, 93);
 			this->button11->TabIndex = 27;
 			this->button11->Text = L"Change Password";
 			this->button11->UseVisualStyleBackColor = true;
 			// 
 			// button10
 			// 
-			this->button10->Location = System::Drawing::Point(435, 493);
-			this->button10->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->button10->Location = System::Drawing::Point(870, 955);
+			this->button10->Margin = System::Windows::Forms::Padding(6, 4, 6, 4);
 			this->button10->Name = L"button10";
-			this->button10->Size = System::Drawing::Size(191, 48);
+			this->button10->Size = System::Drawing::Size(382, 93);
 			this->button10->TabIndex = 26;
 			this->button10->Text = L"Change Phone";
 			this->button10->UseVisualStyleBackColor = true;
 			// 
 			// button9
 			// 
-			this->button9->Location = System::Drawing::Point(435, 369);
-			this->button9->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->button9->Location = System::Drawing::Point(870, 715);
+			this->button9->Margin = System::Windows::Forms::Padding(6, 4, 6, 4);
 			this->button9->Name = L"button9";
-			this->button9->Size = System::Drawing::Size(191, 50);
+			this->button9->Size = System::Drawing::Size(382, 97);
 			this->button9->TabIndex = 25;
 			this->button9->Text = L"Change Email";
 			this->button9->UseVisualStyleBackColor = true;
 			// 
 			// button8
 			// 
-			this->button8->Location = System::Drawing::Point(435, 257);
-			this->button8->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->button8->Location = System::Drawing::Point(870, 498);
+			this->button8->Margin = System::Windows::Forms::Padding(6, 4, 6, 4);
 			this->button8->Name = L"button8";
-			this->button8->Size = System::Drawing::Size(191, 50);
+			this->button8->Size = System::Drawing::Size(382, 97);
 			this->button8->TabIndex = 24;
 			this->button8->Text = L"Change Username";
 			this->button8->UseVisualStyleBackColor = true;
@@ -244,11 +262,12 @@ namespace ProfileGUI {
 			this->label64->AutoSize = true;
 			this->label64->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label64->Location = System::Drawing::Point(59, 601);
+			this->label64->Location = System::Drawing::Point(118, 1164);
+			this->label64->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
 			this->label64->Name = L"label64";
-			this->label64->Size = System::Drawing::Size(212, 29);
+			this->label64->Size = System::Drawing::Size(255, 54);
 			this->label64->TabIndex = 23;
-			this->label64->Text = L"Password: ********";
+			this->label64->Text = L"Password: ";
 			this->label64->Click += gcnew System::EventHandler(this, &ProfilePage::label64_Click);
 			// 
 			// label63
@@ -256,11 +275,12 @@ namespace ProfileGUI {
 			this->label63->AutoSize = true;
 			this->label63->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label63->Location = System::Drawing::Point(59, 493);
+			this->label63->Location = System::Drawing::Point(118, 955);
+			this->label63->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
 			this->label63->Name = L"label63";
-			this->label63->Size = System::Drawing::Size(241, 29);
+			this->label63->Size = System::Drawing::Size(171, 54);
 			this->label63->TabIndex = 22;
-			this->label63->Text = L"Phone: 555-555-5555";
+			this->label63->Text = L"Phone:";
 			this->label63->Click += gcnew System::EventHandler(this, &ProfilePage::label63_Click);
 			// 
 			// label62
@@ -268,29 +288,63 @@ namespace ProfileGUI {
 			this->label62->AutoSize = true;
 			this->label62->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label62->Location = System::Drawing::Point(59, 383);
+			this->label62->Location = System::Drawing::Point(118, 715);
+			this->label62->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
 			this->label62->Name = L"label62";
-			this->label62->Size = System::Drawing::Size(246, 29);
+			this->label62->Size = System::Drawing::Size(152, 54);
 			this->label62->TabIndex = 21;
-			this->label62->Text = L"Email: liam@turing.ca";
+			this->label62->Text = L"Email:";
 			this->label62->Click += gcnew System::EventHandler(this, &ProfilePage::label62_Click);
 			// 
 			// label61
 			// 
 			this->label61->AutoSize = true;
 			this->label61->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14));
-			this->label61->Location = System::Drawing::Point(59, 257);
+			this->label61->Location = System::Drawing::Point(118, 498);
+			this->label61->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
 			this->label61->Name = L"label61";
-			this->label61->Size = System::Drawing::Size(202, 29);
+			this->label61->Size = System::Drawing::Size(253, 54);
 			this->label61->TabIndex = 20;
-			this->label61->Text = L"Username: lfiebig";
+			this->label61->Text = L"Username:";
 			this->label61->Click += gcnew System::EventHandler(this, &ProfilePage::label61_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(418, 498);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(337, 38);
+			this->textBox1->TabIndex = 36;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(418, 715);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(337, 38);
+			this->textBox2->TabIndex = 37;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(418, 955);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(337, 38);
+			this->textBox3->TabIndex = 38;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(418, 1164);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(337, 38);
+			this->textBox4->TabIndex = 39;
 			// 
 			// ProfilePage
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(16, 31);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1602, 903);
+			this->ClientSize = System::Drawing::Size(3204, 1750);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->button12);
 			this->Controls->Add(this->label70);
 			this->Controls->Add(this->label69);
@@ -306,6 +360,7 @@ namespace ProfileGUI {
 			this->Controls->Add(this->label63);
 			this->Controls->Add(this->label62);
 			this->Controls->Add(this->label61);
+			this->Margin = System::Windows::Forms::Padding(6, 6, 6, 6);
 			this->Name = L"ProfilePage";
 			this->Text = L"ProfilePage";
 			this->Load += gcnew System::EventHandler(this, &ProfilePage::ProfilePage_Load);
@@ -316,13 +371,21 @@ namespace ProfileGUI {
 #pragma endregion
 	private: System::Void label64_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
-private: System::Void label62_Click(System::Object^ sender, System::EventArgs^ e) {
+	private: System::Void label62_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
-private: System::Void label61_Click(System::Object^ sender, System::EventArgs^ e) {
+	private: System::Void label61_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
-private: System::Void ProfilePage_Load(System::Object^ sender, System::EventArgs^ e) {
+	private: System::Void ProfilePage_Load(System::Object^ sender, System::EventArgs^ e) {
+		try {
+			String^ currentUserS;
+			currentUserS = marshal_as<String^>(currentUser);
+			MessageBox::Show(currentUserS);
+		}
+		catch (Exception^ ex) {
+			MessageBox::Show(ex->Message);
+		}
 	}
-private: System::Void label63_Click(System::Object^ sender, System::EventArgs^ e) {
+	private: System::Void label63_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
-};
+	};
 }
